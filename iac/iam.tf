6cf28daac58f08db664c07bfb3ba6bec6421c5af
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-06-26 11:12:43 -0300

    new: push api and web to AWS ECR

---
+++ iac/iam.tf
@@ -0,0 +1,80 @@
+resource "aws_iam_openid_connect_provider" "openid_connect_provider" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [
+    var.thumbprint,
+  ]
+  tags = {
+    IaC = "True"
+  }
+}
+
+resource "aws_iam_role" "ecr_role" {
+  name = "ecr_role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              var.gh_app_repo
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "ecr-app-permission"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "apprunner:*"
+          "Effect" : "Allow",
+          "Resource" : "*"
+        },
+        {
+          "Action" : [
+            "iam:PassRole",
+            "iam:CreateServiceLinkedRole"
+          ],
+          "Effect" : "Allow",
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    IaC = "True"
+  }
+}
